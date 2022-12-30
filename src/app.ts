e56098d2fefc0478f7cc95b729e3754e4b30486d
Radeonares32 <bugra.linux.js@gmail.com>
2022-12-30 11:22:06 +0300

    chalk error resolved

--- src/app.ts
+++ src/app.ts
@@ -6,16 +6,18 @@ import path from 'path'
 const app = express()
 const server = http.createServer(app)
 
-app.set('view engine','ejs')
-app.set('views',path.join(path.resolve('./src'),'/views'))
+app.set('view engine', 'ejs')
+app.set('views', path.join(path.resolve('./src'), '/views'))
 
-app.use(express.static(path.join(path.resolve('./src'),'/public')))
+app.use(express.static(path.join(path.resolve('./src'), '/public')))
 
 // Routes
 import { homeRoutes } from './routes/routes'
 
 app.use('/',homeRoutes.getHome)
 
-server.listen(3000,()=>{
+
+
+server.listen(3000, () => {
     console.log(chalk.green("Server running"))
 })
