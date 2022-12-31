22b177cabc99ef43a7294872db54ea8804314ee9
Radeonares32 <bugra.linux.js@gmail.com>
2023-01-01 00:52:11 +0300

    mongo atlas connection

--- src/database/Mongo.db.ts
+++ src/database/Mongo.db.ts
@@ -6,7 +6,8 @@ import { dotenv } from '../configs/config'
 dotenv.dotenvConf()
 export const mongoConnect =  async () => {
     try {
-        await connect(`mongodb://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/?authSource=${process.env.MONGO_DB_NAME}`)
+        await connect(`mongodb+srv://admin:admin@cluster0.3gxnkct.mongodb.net/?retryWrites=true&w=majority`)
+        /* await connect(`mongodb://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/?authSource=${process.env.MONGO_DB_NAME}`) */
 
         console.log(chalk.blue("mongodb connected !!"))
     }
