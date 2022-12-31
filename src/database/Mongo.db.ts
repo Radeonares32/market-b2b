f607722b4029053fd5c8a98fcc0bd1fb5d7aa43d
Radeonares32 <bugra.linux.js@gmail.com>
2022-12-31 14:23:11 +0300

    database mongo with mongoose connect

--- src/database/Mongo.db.ts
+++ src/database/Mongo.db.ts
@@ -1,3 +1,15 @@
 import { connect } from 'mongoose'
+import { dotenv } from '../configs/config'
 
-//mongoose.connect(`mongodb://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/?authSource=${process.env.MONGO_DB_NAME}`)
+//! Dotenv Config
+dotenv.dotenvConf()
+
+export const mongoConnect =  async () => {
+    try {
+        await connect(`mongodb://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/?authSource=${process.env.MONGO_DB_NAME}`)
+    }
+    catch(err) {
+        console.log(err)
+    }
+}
+
