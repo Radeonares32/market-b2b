398581fb842dccaf48e5d4b9554cec7b0650a015
Radeonares32 <bugra.linux.js@gmail.com>
2022-12-31 14:17:47 +0300

    docker and dotenv added

---
+++ src/database/Mongo.db.ts
@@ -0,0 +1,3 @@
+import { connect } from 'mongoose'
+
+//mongoose.connect(`mongodb://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/?authSource=${process.env.MONGO_DB_NAME}`)
