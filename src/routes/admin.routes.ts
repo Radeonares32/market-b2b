b0db13d1c00a0998903e3284c90e740d76d93b6b
Radeonares32 <bugra.linux.js@gmail.com>
2023-01-01 00:08:08 +0300

    admin.routes http request changed

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -6,4 +6,4 @@ import { admin } from '../controllers/controllers'
 
 export const getHome = app.get('/',admin.getHome)
 export const getSign = app.get('/sign',admin.getSign)
-export const postSign = app.get('/sign',admin.postSign)
+export const postSign = app.post('/sign',admin.postSign)
