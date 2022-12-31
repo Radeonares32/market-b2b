c8b504f4705bda933ec8ae1b9fbf69c544bc1427
Radeonares32 <bugra.linux.js@gmail.com>
2022-12-31 14:57:07 +0300

    mongodb connect success

--- src/database/Mongo.db.ts
+++ src/database/Mongo.db.ts
@@ -1,15 +1,16 @@
+import chalk from 'chalk'
 import { connect } from 'mongoose'
 import { dotenv } from '../configs/config'
 
 //! Dotenv Config
 dotenv.dotenvConf()
-
 export const mongoConnect =  async () => {
     try {
         await connect(`mongodb://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/?authSource=${process.env.MONGO_DB_NAME}`)
+
+        console.log(chalk.blue("mongodb connected !!"))
     }
     catch(err) {
         console.log(err)
     }
 }
-
