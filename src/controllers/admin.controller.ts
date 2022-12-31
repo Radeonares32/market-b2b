4dc9cdee9610b90dc6e980efa05fd6ba4041dff2
Radeonares32 <bugra.linux.js@gmail.com>
2023-01-01 00:17:41 +0300

    admin.controller session error solved

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -6,8 +6,9 @@ import { UserModel } from '../models/models'
 export const getHome: Handler = (req, res) => {
     //@ts-ignore
     const admin = req.session.admin
+    console.log(admin)
     if(admin) {
-        res.render('admin/index')
+        res.render('admin/index',{admin})
     }
     else {
         res.redirect('/admin/sign?message=reqSign')
@@ -20,9 +21,11 @@ export const postSign: Handler = async (req, res) => {
     const { email, password } = req.body
     // testing
     const admin = await UserModel.find({ email, password })
+   
     if(admin.length > 0) {
         //@ts-ignore
-        req.session.admin = admin[0].name
+        req.session.admin = admin[0].email
+        
         res.redirect('/admin')
     }
     else {
