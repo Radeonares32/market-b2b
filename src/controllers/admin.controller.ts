4a5d06f7e85da531f26e68ed5c7b131ec765a523
Radeonares32 <bugra.linux.js@gmail.com>
2023-01-01 00:12:16 +0300

    admin auth basic validation for test

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -1,12 +1,31 @@
 import { Handler } from "express";
 
-export const getHome:Handler = (req,res) => {
-    res.render('admin/index')
+//Models for testing
+import { UserModel } from '../models/models'
+
+export const getHome: Handler = (req, res) => {
+    //@ts-ignore
+    const admin = req.session.admin
+    if(admin) {
+        res.render('admin/index')
+    }
+    else {
+        res.redirect('/admin?message=reqSign')
+    }
 }
-export const getSign:Handler = (req,res) => {
+export const getSign: Handler = (req, res) => {
     res.render('admin/sign')
 }
-export const postSign:Handler = (req,res) => {
-    const {email,password} = req.body
-    console.log(req.body)
+export const postSign: Handler = async (req, res) => {
+    const { email, password } = req.body
+    // testing
+    const admin = await UserModel.find({ email, password })
+    if(admin.length > 0) {
+        //@ts-ignore
+        req.session.admin = admin[0].name
+        res.redirect('/admin')
+    }
+    else {
+        res.redirect('/admin?message=noUser')
+    }
 }
