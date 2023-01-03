7a4fe57f1e6206bec3004b0788b046c144a682d2
Radeonares32 <bugra.linux.js@gmail.com>
2023-01-04 01:05:50 +0300

    type for user

---
+++ src/services/user/types/user.types.ts
@@ -0,0 +1,12 @@
+export interface UserTypes {
+     name?: string;
+     surname?: string
+     email?: string
+     password?: string | any
+     image?: string
+     isAdmin?: boolean
+     cardName?: string
+     cardLastName?: string
+     cardNumber?: number
+     cardCvv?: number
+}
