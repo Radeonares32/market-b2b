3cf5e74a570cb6d151133cafc1cc76109012bba5
Radeoanres32 <bugra.linux.js@gmail.com>
2023-01-07 00:04:09 +0300

    changed user login rename

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -7,9 +7,9 @@ import { user } from '../controllers/controllers'
 
 //Get
 export const getSign = app.get('/sign',user.getSign)
-export const getLogin = app.get('/login',user.getLogin)
+export const getLogin = app.get('/login',user.getSignup)
 export const getLogout = app.get('/logout',user.getLogout)
 
 //Post
-export const postLogin = app.post('/login',user.postLogin)
+export const postLogin = app.post('/login',user.postSignup)
 export const postSign = app.post('/sign',user.postSign)
