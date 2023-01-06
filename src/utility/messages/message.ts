17b216c905d080bad8a1bae922c7569d68262333
Radeoanres32 <bugra.linux.js@gmail.com>
2023-01-06 19:13:54 +0300

    Message class updated

--- src/utility/messages/message.ts
+++ src/utility/messages/message.ts
@@ -1,22 +1,30 @@
-import { IMessage,MessageType } from './types/message.type'
+import {MessageType,MessageTypes} from './types/message.type'
+import {IMessage} from './interface/message.interface'
 
 export class Message implements IMessage {
-    private messageType:string
-    private messageContext:string
-    constructor(messageType:string,messageContext:string){
+    readonly messageType:MessageTypes
+    readonly  messageContext:string
+    constructor(messageType:MessageTypes,messageContext:string){
         this.messageType = messageType
         this.messageContext = messageContext
     }
-    public InformationMessage() {
+    public InformationMessage():MessageType {
         return {
             messageType:this.messageType,
             messageContext:this.messageContext
         }
     }
-    public SuccessMessage: () => MessageType;
-    public ErrorMessage: () => MessageType;
-
+    public SuccessMessage():MessageType {
+        return {
+            messageType:this.messageType,
+            messageContext:this.messageContext
+        }
+    };
+    public ErrorMessage():MessageType {
+        return {
+            messageType:this.messageType,
+            messageContext:this.messageContext
+        }
+    }
 }
-
-const x = new Message()
-const y = x.InformationMessage({messageType:"",messageContext:""})
+const m = new Message("SUCCESS", "asd")
